558ffd7d115df57dab7371350a88e8f558d769e7
ArifKhosa123 <arif.alimuhammad@akhuwat.org.pk>
2026-03-01 06:39:52 +0000

    add ci pipeline

--- DevOpsApi/obj/Debug/net10.0/DevOpsApi.AssemblyInfo.cs
+++ DevOpsApi/obj/Debug/net10.0/DevOpsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevOpsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df188bd7d62b559f90862c9995237aa89f8ac86e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4dec3979507c6bfb7229e973ce731bba177a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevOpsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevOpsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
